f370489b4cd22f2cce2efa04f936f259d38e3e23
Pravuswain <42239038+Pravuswain@users.noreply.github.com>
2018-08-17 05:38:24 +0000

    Add files via upload

---
+++ lab3q2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+int c,d,e,f;
+float g;
+cout<<"c=6+9"<<"\n";
+cout<<"d=2+(-3)"<<"\n";
+cout<<"e=4*5"<<"\n";
+cout<<"f=8%7"<<"\n";
+cout<<"g=1/9"<<"\n";
+return 0;
+}
